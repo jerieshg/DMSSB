5fc36f42c9fcd437cf8f875bb10d3898254d5d4f
Jeries Handal <JeriesHG@gmail.com>
2017-06-27 14:48:21 -0600

    Added backup files

---
+++ backup/mongo_backup.sh
@@ -0,0 +1,19 @@
+MONGO_DATABASE="enersa"
+APP_NAME="enersa-dsm"
+
+MONGO_HOST="127.0.0.1"
+MONGO_PORT="27017"
+TIMESTAMP=`date +%F-%H%M`
+MONGODUMP_PATH="/usr/bin/mongodump"
+BACKUPS_DIR="/home/jeries/backups/$APP_NAME"
+BACKUP_NAME="$APP_NAME-$TIMESTAMP"
+ 
+# mongo admin --eval "printjson(db.fsyncLock())"
+# $MONGODUMP_PATH -h $MONGO_HOST:$MONGO_PORT -d $MONGO_DATABASE
+$MONGODUMP_PATH -d $MONGO_DATABASE
+# mongo admin --eval "printjson(db.fsyncUnlock())"
+ 
+mkdir -p $BACKUPS_DIR
+mv dump $BACKUP_NAME
+tar -zcvf $BACKUPS_DIR/$BACKUP_NAME.tgz $BACKUP_NAME
+rm -rf $BACKUP_NAME
